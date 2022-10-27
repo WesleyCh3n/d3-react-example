4453b838876b0252ff71d41da82089ad664e6271
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-10-27 16:21:03 +0800

    ui: add control label

--- src/App.tsx
+++ src/App.tsx
@@ -16,20 +16,31 @@ function App() {
   };
   const [layout, setLayout] = useState<Layout>(initLayout);
 
+  const shuffleData = () => {
+    let d = d3.range(20).map(i => {
+      return { x: i, y: d3.randomUniform(d3.randomUniform(20)())() };
+    });
+    setData(() => d);
+
+  }
+
   return (
-    <div className="p-2 flex">
-      <div className="p-2">
-        <h1 className="text-center">SVG Line Plot</h1>
-        <LinePlot data={data} layout={layout} setData={setData} />
-        <div className="p-2 flex justify-center gap-4">
+    <div className="p-2">
+      <div className="flex justify-center">
+        <div className="p-2">
+          <h1 className="text-center">SVG Line Plot</h1>
+          <LinePlot data={data} layout={layout} setData={setData} />
+        </div>
+        <div className="p-2">
+          <h1 className="text-center">Canvas Plot</h1>
+          <CanvasPlot data={data} layout={layout} />
+        </div>
+      </div>
+      <div className="p-2 flex flex-col items-center gap-2">
+        <div className="space-x-2">
           <button
             className="border p-2 rounded hover:bg-slate-200"
-            onClick={() => {
-              let a = d3.range(20).map(i => {
-                return { x: i, y: d3.randomUniform(d3.randomUniform(20)())() };
-              });
-              setData(a);
-            }}
+            onClick={shuffleData}
           >
             shuffle
           </button>
@@ -39,30 +50,9 @@ function App() {
           >
             clear
           </button>
-          <input
-            type="number"
-            className="border p-2 rounded w-14 text-center font-bold"
-            step={1}
-            value={layout.left}
-            onChange={e =>
-              setLayout(layout => ({ ...layout, left: +e.target.value }))}
-          />
-          <input
-            type="number"
-            className="border p-2 rounded w-14 text-center font-bold"
-            step={1}
-            value={layout.bottom}
-            onChange={e =>
-              setLayout(layout => ({ ...layout, bottom: +e.target.value }))}
-          />
-          <input
-            type="number"
-            className="border p-2 rounded w-12 text-center font-bold"
-            step={1}
-            value={layout.right}
-            onChange={e =>
-              setLayout(layout => ({ ...layout, right: +e.target.value }))}
-          />
+        </div>
+        <div className="space-x-2">
+          {"top: "}
           <input
             type="number"
             className="border p-2 rounded w-12 text-center font-bold"
@@ -71,31 +61,40 @@ function App() {
             onChange={e =>
               setLayout(layout => ({ ...layout, top: +e.target.value }))}
           />
+          {" bottom: "}
+          <input
+            type="number"
+            className="border p-2 rounded w-14 text-center font-bold"
+            step={1}
+            value={layout.bottom}
+            onChange={e =>
+              setLayout(layout => ({ ...layout, bottom: +e.target.value }))}
+          />
+          {" left: "}
+          <input
+            type="number"
+            className="border p-2 rounded w-14 text-center font-bold"
+            step={1}
+            value={layout.left}
+            onChange={e =>
+              setLayout(layout => ({ ...layout, left: +e.target.value }))}
+          />
+          {" right: "}
+          <input
+            type="number"
+            className="border p-2 rounded w-12 text-center font-bold"
+            step={1}
+            value={layout.right}
+            onChange={e =>
+              setLayout(layout => ({ ...layout, right: +e.target.value }))}
+          />
           <button
             className="border p-2 rounded hover:bg-slate-200"
             onClick={() => setLayout(initLayout)}
           >
             reset
           </button>
-          <button
-            onClick={() => {
-              console.log({ data });
-            }}
-          >
-            data
-          </button>
         </div>
-      </div>
-      <div className="p-2">
-        <h1 className="text-center">Canvas Plot</h1>
-        <CanvasPlot data={data} layout={{
-          width: 600,
-          height: 400,
-          left: 25,
-          right: 0,
-          top: 0,
-          bottom: 25,
-        }} />
       </div>
     </div>
   );
